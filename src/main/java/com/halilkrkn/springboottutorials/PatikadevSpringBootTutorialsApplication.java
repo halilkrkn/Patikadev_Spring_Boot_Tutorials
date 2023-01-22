bcc1ce6ec3763c63d1102cb69e6937d2389a1345
halilkrkn <Halilkrkn000@gmail.com>
2023-01-22 22:47:14 +0300

    Initial commit

---
+++ src/main/java/com/halilkrkn/springboottutorials/PatikadevSpringBootTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.halilkrkn.springboottutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikadevSpringBootTutorialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatikadevSpringBootTutorialsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/halilkrkn/springboottutorials/PatikadevSpringBootTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halilkrkn.springboottutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikadevSpringBootTutorialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
